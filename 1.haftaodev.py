f00a90fca95978b4726b17fc1fa78573eca8b1d6
aytencaliskann <112652789+aytencaliskann@users.noreply.github.com>
2022-09-06 23:51:25 +0300

    Add files via upload

---
+++ 1.haftaodev.py
@@ -0,0 +1,16 @@
+#SORU: KLAVYEDEN GİRİLEN 2 SAYIDAN 1.NİN BÜYÜK OLUP OLMADIĞINI EKRANA YAZDIRAN KOD
+"""
+sayi1= int(input("Birinci sayıyı giriniz: "))
+sayi2= int(input("İkinci sayıyı giriniz:  "))
+if sayi1 > sayi2:
+ print("sayi1 büyüktür sayi2den")
+else:
+ print("sayi1 küçüktür sayi2 den")
+"""
+#SORU: KULLANICIDAN ALINAN 1.SAYININ KULLANICININ GİRDİĞİ 2.SAYIYA KUVVETİNİ HESAPLAYARAK EKRANA YAZDIRAN PROGRAM
+
+sayi1=int(input("Birinci sayıyı giriniz: "))
+sayi2=int(input("ikinci sayıyı giriniz: "))
+
+hesap=sayi1**sayi2
+print(hesap)
